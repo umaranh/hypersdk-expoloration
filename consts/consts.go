0f98a109529a53107299d99ac2bc7a68ad743de4
umaranh <umaran763@gmail.com>
2024-12-12 21:46:36 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "umaran"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "umachain"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "UMC"
 )
 
 var ID ids.ID
